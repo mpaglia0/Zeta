133c886404beee342c3ad3c288a43118bc04bcfb
Maurizio Paglia <mpaglia0@gmail.com>
2021-02-26 17:19:58 +0100

    Stringhe di messaggio tradotte in italiano

--- static/mail/contact_me.php
+++ static/mail/contact_me.php
@@ -2,23 +2,23 @@
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
+   empty($_POST['website'])   ||
    empty($_POST['message'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-   echo "No arguments Provided!";
+   echo "Non hai completato tutti i campi!";
    return false;
    }
    
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$website = strip_tags(htmlspecialchars($_POST['website']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
 $to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_subject = "Hai un nuovo messaggio da $name";
+$email_body = "Hai ricevuto un nuovo messaggio dal tuo sito web.\n\n"."Ecco i dettagli:\n\nNome: $name\n\nEmail: $email_address\n\nWebsite: $website\n\nMessaggio:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
