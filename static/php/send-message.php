b30a858e607f3fb5f2b8ea30c769aa315c25916d
Maurizio Paglia <mpaglia0@gmail.com>
2021-04-28 11:05:41 +0200

    Update send-message.php

--- static/php/send-message.php
+++ static/php/send-message.php
@@ -6,7 +6,7 @@ if(empty($_POST['name'])   ||
    empty($_POST['msg'])    ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-   echo "Per favore, compila tutti i campi richiesti!";
+   echo "Please fill all requested fields!";
    return false;
    }
 
@@ -16,9 +16,9 @@ $subject = strip_tags(htmlspecialchars($_POST['subj']));
 $message = strip_tags(htmlspecialchars($_POST['msg']));
    
 // Create the email and send the message
-$to = 'info@mauriziopaglia.it'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Nuovo messaggio ($subject)";
-$email_body = "Hai ricevuto un messaggio dal sito.\n\n"."Qui sotto ci sono tutti i dettagli:\n\nNome: $name\n\nCommento:\n$message";
+$to = 'you@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "New message ($subject)";
+$email_body = "You have a new message!\n\n"."All details here below:\n\nName: $name\n\nComment:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
