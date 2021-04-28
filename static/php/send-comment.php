e6714cc90fb400d3a093d8d978691af068323795
Maurizio Paglia <mpaglia0@gmail.com>
2021-04-28 12:11:58 +0200

    Update send-comment.php

--- static/php/send-comment.php
+++ static/php/send-comment.php
@@ -19,8 +19,6 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "New comment from $name";
 $email_body = "You received a new comment on your website.\n\n"."All details are here below:\n\nName: $name\n\nEmail: $email_address\n\nWebsite: $website\n\nComment:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
+mail($to,$email_subject,$email_body);
 return true;         
 ?>
