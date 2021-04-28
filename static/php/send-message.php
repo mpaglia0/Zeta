d6a3dfba95c289b3818d6b75a5a91b492913472b
Maurizio Paglia <mpaglia0@gmail.com>
2021-04-28 12:12:37 +0200

    Update send-message.php

--- static/php/send-message.php
+++ static/php/send-message.php
@@ -19,8 +19,6 @@ $message = strip_tags(htmlspecialchars($_POST['msg']));
 $to = 'you@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "New message ($subject)";
 $email_body = "You have a new message!\n\n"."All details here below:\n\nName: $name\n\nComment:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
+mail($to,$email_subject,$email_body);
 return true;         
 ?> 
